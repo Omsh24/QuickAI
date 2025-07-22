58526a57eda497d27b2b312fef0e37b7dbbfbde8
Om Shukla <omshukla2403@gmail.com>
2025-07-22 18:12:04 +0530

    Server fix

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -3,6 +3,8 @@ import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
 import axios from "axios";
 import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+import pdf from 'pdf-parse/lib/pdf-parse.js';
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -144,6 +146,143 @@ export const generateImage = async (req, res) => {
         res.json({
             success: true,
             content: secure_url
+        });
+
+    } catch (error) {
+        console.error("Error generating article:", error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });   
+    }
+}
+
+export const removeImageBackground = async (req, res) => {
+    try {
+        const {userId} = req.auth();
+        const image = req.file;
+        const plan = req.plan;
+
+        if(plan !== 'premium') {
+            return res.status(403).json({
+                success: false,
+                message: 'This feature is only available for premium users.'
+            });
+        }
+
+        const {secure_url} = await cloudinary.uploader.upload(image.path, {
+            transformation: [
+                { 
+                    effect: "background_removal",
+                    background_removal: "remove_the_background",
+                }
+            ]
+        })
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type)
+        VALUES (${userId}, 'Remove background from image', ${secure_url}, 'image')`;
+
+        res.json({
+            success: true,
+            content: secure_url
+        });
+
+    } catch (error) {
+        console.error("Error generating article:", error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });   
+    }
+}
+
+export const removeImageObject = async (req, res) => {
+    try {
+        const {userId} = req.auth();
+        const {object} = req.body;
+        const image = req.file;
+        const plan = req.plan;
+
+        if(plan !== 'premium') {
+            return res.status(403).json({
+                success: false,
+                message: 'This feature is only available for premium users.'
+            });
+        }
+
+        const {secure_url} = await cloudinary.uploader.upload(image.path)
+
+        const imageUrl = cloudinary.url(secure_url, {
+            transformation: [
+                { 
+                    effect: `gen_remove:${object}`
+                }
+            ],
+            resource_type: 'image'
+        })
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type)
+        VALUES (${userId}, ${`Removed ${object} from Image.`}, ${imageUrl}, 'image')`;
+
+        res.json({
+            success: true,
+            content: imageUrl
+        });
+
+    } catch (error) {
+        console.error("Error generating article:", error);
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });   
+    }
+}
+
+export const resumeReview = async (req, res) => {
+    try {
+        const {userId} = req.auth();
+        const resume = req.file;
+        const plan = req.plan;
+
+        if(plan !== 'premium') {
+            return res.status(403).json({
+                success: false,
+                message: 'This feature is only available for premium users.'
+            });
+        }
+
+        if(resume.size > 5*1024*1024){
+            return res.status(400).json({
+                success: false,
+                message: 'Resume file size exceeds the 5MB limit.'
+            });
+        }
+
+        const dataBuffer = fs.readFileSync(resume.path)
+        const pdfData = await pdf(dataBuffer);
+
+        const prompt = `Review the following resume and provide constructive feedback on its strength, weakness , and areas for improvement. The resume content is as follows:\n\n${pdfData.text }`;
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: 1000,
+        });
+        
+        const content = response.choices[0].message.content;
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type)
+        VALUES (${userId}, 'Review the uploadded Resume', ${content}, 'review-resume')`;
+
+        res.json({
+            success: true,
+            content: content
         });
 
     } catch (error) {
